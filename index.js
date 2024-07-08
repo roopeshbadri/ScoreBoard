881d36f5f21d4602ded0c61874a4f89ead131ec8
roopeshbadri <badriroopesh@gmail.com>
2024-07-09 00:35:18 +0530

    Added the files of the ScoreBoard application

---
+++ index.js
@@ -0,0 +1,29 @@
+let homeScoreEl = document.getElementById("home-scoreel")
+let homeScore = 0
+let awayScoreEl = document.getElementById("away-scoreel")
+let awayScore = 0
+const oneHome = () => {
+    homeScore += 1;
+    homeScoreEl.innerHTML=homeScore
+}
+const twoHome = () => {
+    homeScore += 2;
+    homeScoreEl.innerHTML = homeScore
+}
+const threeHome = () => {
+    homeScore += 3;
+    homeScoreEl.innerHTML = homeScore
+}
+const oneAway = () => {
+    awayScore += 1;
+    awayScoreEl.innerHTML = awayScore
+}
+const twoAway = () => {
+    awayScore += 2;
+    awayScoreEl.innerHTML = awayScore
+}
+const threeAway = () => {
+    awayScore += 3;
+    awayScoreEl.innerHTML = awayScore
+}
+
